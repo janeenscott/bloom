58438238aeb185b235274e7e317272c04cb5492d
Janeen Scott <janeen.e.scott@gmail.com>
2019-02-02 16:03:59 -0500

    tried to move all views into one... broke some things...moved them back

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,9 +2,8 @@
 accounts app urls
 """
 
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
